4059171e83a3317602b4c65ad8b3765912161167
M4rm3ladov <71574209+M4rm3ladov@users.noreply.github.com>
2021-03-09 19:31:09 +0800

    Needs and Satisfaction
    Added scripts for character needs and satisfaction

---
+++ Hygiene Mobile/Assets/Scripts/Controllers/CharController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _characterAnimator;
+    private void Awake() {
+
+    }
+    #region Animation
+    public void Walk(){
+        _characterAnimator.SetTrigger("Walk");
+    }
+    public void Eat(){
+        _characterAnimator.SetTrigger("Eat");
+    }
+    public void Wave(){
+        _characterAnimator.SetTrigger("Happy");
+    }
+    #endregion
+
+}
---
+++ Hygiene Mobile/Assets/Scripts/Managers/CharManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharManager : MonoBehaviour
+{
+    CharController charController;
+    NeedsController needsController;
+    private void Awake() {
+        
+    }
+    #region Consequences
+    public static void Famished()
+    {
+        Debug.Log("Famished");
+    }
+    public static void Drowsed()
+    {
+        Debug.Log("Drowsed");
+    }
+    public static void Dirty()
+    {
+        Debug.Log("Dirty");
+    }
+    #endregion
+}

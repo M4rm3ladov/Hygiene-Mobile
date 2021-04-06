e45edf0a726b4883d6bf3d4b7d6132be3823519f
M4rm3ladov <71574209+M4rm3ladov@users.noreply.github.com>
2021-04-06 12:43:10 +0800

    added sleep anim, made drawer clickable, rearranged Player prefab and reverted player settings

---
+++ Hygiene Mobile/Assets/Scripts/Controllers/DrawerController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerController : MonoBehaviour
+{
+   private void OnMouseDown() {
+       Debug.Log("Tap");
+   }
+}
---
+++ Hygiene Mobile/Assets/Scripts/Controllers/LampController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject RoomLight;
+    [SerializeField]
+    private GameObject Body;
+    [SerializeField]
+    private GameObject SleepHead;
+    private int _lightState = 0;
+    
+    private void OnMouseDown() {
+        if(_lightState == 0)
+        {
+            RoomLight.SetActive(true);
+            Body.SetActive(false);
+            _lightState = 1; 
+
+            SpriteRenderer[] sprites = SleepHead.GetComponentsInChildren<SpriteRenderer>();
+ 
+            for(int i = 0; i < sprites.Length; i++){
+            sprites[i].enabled = true;
+            }
+        }
+        else if(_lightState == 1)
+        {
+            RoomLight.SetActive(false);
+            Body.SetActive(true);
+            _lightState = 0;
+
+            SpriteRenderer[] sprites = SleepHead.GetComponentsInChildren<SpriteRenderer>();
+ 
+            for(int i = 0; i < sprites.Length; i++){
+            sprites[i].enabled = false;
+            }
+        }               
+    }
+}

99a690618d932303497c348920f953b19a12e169
M4rm3ladov <71574209+M4rm3ladov@users.noreply.github.com>
2021-04-13 01:38:36 +0800

    scene transition working

--- Hygiene Mobile/Assets/TransitionController.cs
+++ Hygiene Mobile/Assets/TransitionController.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TransitionController : MonoBehaviour
 {
     public Animator transition;
     [SerializeField]
     private float _transitionTime = 1f;
+    [SerializeField]
+    private string _levelName;
     // Update is called once per frame
     private void OnMouseDown() {
         LoadNextLevel();
     }
     public void LoadNextLevel()
     {
-        StartCoroutine(LoadLevel());
+        StartCoroutine(LoadLevel(_levelName));
     }
 
-    IEnumerator LoadLevel(){
+    IEnumerator LoadLevel(string levelName){
         transition.SetTrigger("Start");
         yield return new WaitForSeconds(_transitionTime);
-        transition.SetTrigger("End");
-        yield return new WaitForSeconds(_transitionTime);
+        SceneManager.LoadScene(levelName);
     }
 }

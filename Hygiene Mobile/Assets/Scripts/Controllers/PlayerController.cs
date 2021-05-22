cdf9d5859d16414337e19163c91f000b3ba43e54
M4rm3ladov <71574209+M4rm3ladov@users.noreply.github.com>
2021-05-22 09:53:49 +0800

    added bubble messages prefabs, moved DraggableFoodController

--- Hygiene Mobile/Assets/Scripts/Controllers/PlayerController.cs
+++ Hygiene Mobile/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private Image cleanBubble;
+    [SerializeField]
+    private Image hungerBubble;
+    [SerializeField]
+    private Image tiredBubble;
     [SerializeField]
     private int tiredTrigger;
     [SerializeField]
@@ -22,6 +29,8 @@ public class PlayerController : MonoBehaviour
         get{ return animTransition; }
         set{ animTransition = value; }
     }
+    private float fadingLength = 10f;
+    private float currTime;
     
     private void Start() {
         //play sleepy anim if other animation is playing and if energy is below/equal to alloted thereshold
@@ -33,26 +42,33 @@ public class PlayerController : MonoBehaviour
         }else{
             animTransition.SetBool("isHungrySleepy", false);
         }*/
+        hungerBubble.CrossFadeAlpha(0, 0.001f, true);
         //hunger
-        if((int)Player.Hunger <= hungerTrigger &&
+        /*if((int)Player.Hunger <= hungerTrigger &&
         //!animTransition.GetCurrentAnimatorStateInfo(0).IsName("HungrySleepy") ||
         !animTransition.GetCurrentAnimatorStateInfo(0).IsName("Dirty"))
         {
-            animTransition.SetBool("isHungry", true);   
+            animTransition.SetBool("isHungry", true); 
+
+            hungerBubble.enabled = true;  
+            hungerBubble.CrossFadeAlpha(0, 0.5f, true);       
         }else{
             animTransition.SetBool("isHungry", false);
-        }
+        }*/
+        PlayStopHungerAnimation();
+        PlayStopSleepyAnimation();
         //sleepy
-        if((int)Player.Energy <= tiredTrigger &&
+        /*if((int)Player.Energy <= tiredTrigger &&
         //!animTransition.GetCurrentAnimatorStateInfo(0).IsName("HungrySleepy") ||
         !animTransition.GetCurrentAnimatorStateInfo(0).IsName("Hungry"))
         {
             animTransition.SetBool("isSleepy", true);   
         }else{
             animTransition.SetBool("isSleepy", false);
-        }      
+        } */     
     }
     private void Update() {
+        TimeFadeUpdate();
         //play sleepy anim if other animation is playing and if energy is below/equal to alloted thereshold
         //else turn it off
         /*if((int)Player.Energy <= tiredTrigger && (int)Player.Hunger <= hungerTrigger &&
@@ -63,25 +79,50 @@ public class PlayerController : MonoBehaviour
             animTransition.SetBool("isHungrySleepy", false);
         }*/
         //hunger
+        PlayStopHungerAnimation();
+        //sleepy
+        PlayStopSleepyAnimation();    
+    }
+    private void TimeFadeUpdate(){
+        if (currTime <= 0)
+        {
+            currTime = fadingLength;
+            return;
+        }
+        currTime -= Time.deltaTime;
+    }
+    private void PlayStopHungerAnimation(){
         if((int)Player.Hunger <= hungerTrigger &&
-        !animTransition.GetCurrentAnimatorStateInfo(0).IsName("Dirty") &&
-        //!animTransition.GetCurrentAnimatorStateInfo(0).IsName("HungrySleepy") ||  
+        !animTransition.GetCurrentAnimatorStateInfo(0).IsName("Dirty") && 
         !animTransition.GetCurrentAnimatorStateInfo(0).IsName("Wave"))
         {
-            animTransition.SetBool("isHungry", true);       
-        }else{
-            animTransition.SetBool("isHungry", false);
+            animTransition.SetBool("isHungry", true);  
+            FadeInOutBubble(hungerBubble);   
+            return;    
         }
-        //sleepy
+        animTransition.SetBool("isHungry", false);
+        hungerBubble.CrossFadeAlpha(0, 0.5f, true);
+        hungerBubble.enabled = false;
+    }
+    private void PlayStopSleepyAnimation(){
         if((int)Player.Energy <= tiredTrigger &&
         !animTransition.GetCurrentAnimatorStateInfo(0).IsName("Hungry") &&
-        //!animTransition.GetCurrentAnimatorStateInfo(0).IsName("HungrySleepy") ||
         !animTransition.GetCurrentAnimatorStateInfo(0).IsName("Wave"))
         {
-            animTransition.SetBool("isSleepy", true);       
-        }else{
-            animTransition.SetBool("isSleepy", false);
-        }    
+            animTransition.SetBool("isSleepy", true); 
+            return;      
+        }
+        animTransition.SetBool("isSleepy", false);
+    }
+    private void FadeInOutBubble(Image bubble)
+    {
+        bubble.enabled = true;
+        if(currTime < 5f){
+            bubble.CrossFadeAlpha(0, 0.5f, true);
+            return;
+        }          
+        //if(currTime <= 10f && currTime >= 5f){ 
+        bubble.CrossFadeAlpha(1, 0.5f, true);
     }
     //wave on character click
     private void OnMouseDown() {
